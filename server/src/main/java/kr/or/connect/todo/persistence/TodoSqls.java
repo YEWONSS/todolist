11984bd8bc08e539ed2e2862c2787c47d8a55fa4
YEWONSS <syw0930@naver.com>
2017-06-06 23:38:38 +0900

    Add files via upload

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -1,6 +1,18 @@
 package kr.or.connect.todo.persistence;
 
 public class TodoSqls {
-	static final String DELETE_BY_ID =
+	static final String DELETE_BY_ID=
 			"DELETE FROM todo WHERE id= :id";
+	
+	static final String SELECT_ALL= 
+			"SELECT id, todo, completed, date FROM todo";
+	
+	static final String UPDATE=
+			"UPDATE todo SET completed= :completed WHERE id= :id";
+	
+	static final String SELECT_BY_ID=
+			"SELECT id, todo, completed, date FROM todo WHERE id= :id";
+	
+	static final String COUNT= 
+			"SELECT COUNT(*) FROM todo";
 }
